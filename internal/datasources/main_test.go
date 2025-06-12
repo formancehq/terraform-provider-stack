aebf0eeab5c6adc3b7df63e1da25e1544b45073e
David Ragot <35502263+Dav-14@users.noreply.github.com>
2025-06-12 15:39:53 +0200

    style: lint

--- internal/datasources/main_test.go
+++ internal/datasources/main_test.go
@@ -1,16 +1,9 @@
 package datasources_test
 
-import (
-	"context"
-	"testing"
+// func test(t *testing.T, fn func(ctx context.Context)) {
+// 	t.Parallel()
 
-	"github.com/formancehq/go-libs/v3/logging"
-)
+// 	ctx := logging.TestingContext()
 
-func test(t *testing.T, fn func(ctx context.Context)) {
-	t.Parallel()
-
-	ctx := logging.TestingContext()
-
-	fn(ctx)
-}
+// 	fn(ctx)
+// }
