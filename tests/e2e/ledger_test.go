bd04f562dd6eb033577699303976fbf529cb3601
David Ragot <35502263+Dav-14@users.noreply.github.com>
2025-06-17 17:58:57 +0200

    wip:

--- tests/e2e/ledger_test.go
+++ tests/e2e/ledger_test.go
@@ -6,6 +6,9 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
 	"github.com/hashicorp/terraform-plugin-testing/tfversion"
 )
 
@@ -40,6 +43,45 @@ func TestLedger(t *testing.T) {
 							}
 						}
 					`,
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue("formancestack_ledger.default", tfjsonpath.New("name"), knownvalue.StringExact("test")),
+					statecheck.ExpectKnownValue("formancestack_ledger.default", tfjsonpath.New("bucket"), knownvalue.StringExact("test-bucket")),
+					statecheck.ExpectKnownValue("formancestack_ledger.default", tfjsonpath.New("metadata"), knownvalue.MapExact(
+						map[string]knownvalue.Check{
+							"key1": knownvalue.StringExact("value1"),
+							"key2": knownvalue.StringExact("value2"),
+						},
+					)),
+				},
+			},
+			{
+				Config: newStack(OrganizationId, RegionName) +
+					`
+						provider "formancestack" {
+							stack_id = formancecloud_stack.default.id
+							organization_id = formancecloud_stack.default.organization_id
+							uri = formancecloud_stack.default.uri
+						}
+
+						resource "formancestack_ledger" "default" {
+							name = "test"
+							bucket = "test-bucket"
+							metadata = {
+								"key1" = "value1"
+								"key2" = "newvalue"
+							}
+						}
+					`,
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue("formancestack_ledger.default", tfjsonpath.New("name"), knownvalue.StringExact("test")),
+					statecheck.ExpectKnownValue("formancestack_ledger.default", tfjsonpath.New("bucket"), knownvalue.StringExact("test-bucket")),
+					statecheck.ExpectKnownValue("formancestack_ledger.default", tfjsonpath.New("metadata"), knownvalue.MapExact(
+						map[string]knownvalue.Check{
+							"key1": knownvalue.StringExact("value1"),
+							"key2": knownvalue.StringExact("newvalue"),
+						},
+					)),
+				},
 			},
 			{
 				RefreshState: true,
