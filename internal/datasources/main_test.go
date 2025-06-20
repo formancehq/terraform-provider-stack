f7450061334f40eb486a8d755245d73592c83e86
David Ragot <35502263+Dav-14@users.noreply.github.com>
2025-06-20 14:27:33 +0200

    feat: add integration test

--- internal/datasources/main_test.go
+++
@@ -1,9 +0,0 @@
-package datasources_test
-
-// func test(t *testing.T, fn func(ctx context.Context)) {
-// 	t.Parallel()
-
-// 	ctx := logging.TestingContext()
-
-// 	fn(ctx)
-// }
---
+++ tests/integration/main_test.go
@@ -0,0 +1,9 @@
+package integration
+
+import "testing"
+
+func TestMain(t *testing.T) {
+	// This is a placeholder for the main test function.
+	// The actual tests will be defined in separate files.
+	t.Skip("Integration tests are not implemented yet")
+}
