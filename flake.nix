1690972df9db5cd9387206dae48dddd235fba2cc
David Ragot <35502263+Dav-14@users.noreply.github.com>
2026-02-18 18:26:20 +0100

    feat(speakeasy): add missing binary

--- flake.nix
+++ flake.nix
@@ -33,6 +33,20 @@
           f { pkgs = pkgs; system = system; }
         );
 
+      speakeasyVersion = "1.690.0";
+      speakeasyPlatforms = {
+        "x86_64-linux"   = "linux_amd64";
+        "aarch64-linux"  = "linux_arm64";
+        "x86_64-darwin"  = "darwin_amd64";
+        "aarch64-darwin" = "darwin_arm64";
+      };
+      speakeasyHashes = {
+        "x86_64-linux"   = "d0943a498f8df18fe85097f514d3b900beb56738f93e760235f8180def1bbd0a";
+        "aarch64-linux"  = "cbdcfc032735c17b71714622743510737811ece0a4cd540ef4a6e6b46ec65075";
+        "x86_64-darwin"  = "ee8806f46fdc6655a957f5220f7904b6de69afd1f96f7865de1bfecd92054541";
+        "aarch64-darwin" = "33730320d768c89bf631fdc2bc8fd3d2c425c3a1015ae4224495ef91e3f883d7";
+      };
+      
     in
     {
       overlays.default = final: prev: {
@@ -41,9 +55,35 @@
 
       packages = forEachSupportedSystem ({ pkgs, system }:
         {
+          speakeasy = pkgs.stdenv.mkDerivation {
+            pname = "speakeasy";
+            version = speakeasyVersion;
 
+            src = pkgs.fetchurl {
+              url = "https://github.com/speakeasy-api/speakeasy/releases/download/v${speakeasyVersion}/speakeasy_${speakeasyPlatforms.${system}}.zip";
+              sha256 = speakeasyHashes.${system};
+            };
+
+            nativeBuildInputs = [ pkgs.unzip ];
+            dontUnpack = true;
+
+            installPhase = ''
+              mkdir -p $out/bin
+              unzip $src
+              ls -al
+              install -m755 speakeasy $out/bin/
+            '';
+
+            name = "speakeasy";
+          };
         }
       );
+
+
+      defaultPackage.x86_64-linux   = self.packages.x86_64-linux.speakeasy;
+      defaultPackage.aarch64-linux  = self.packages.aarch64-linux.speakeasy;
+      defaultPackage.x86_64-darwin  = self.packages.x86_64-darwin.speakeasy;
+      defaultPackage.aarch64-darwin = self.packages.aarch64-darwin.speakeasy;
 
       devShells = forEachSupportedSystem ({ pkgs, system }:
         {
@@ -57,6 +97,7 @@
               yq-go
               jq
               pkgs.nur.repos.goreleaser.goreleaser-pro
+              self.packages.${system}.speakeasy
               mockgen
               gomarkdoc
               just
